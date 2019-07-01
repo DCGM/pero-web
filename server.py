0e15cfbe41c8c8e8fa1ee488c389c1f0e021725b
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2019-07-02 01:41:42 +0200

    Add bmod_lines.zip

--- server.py
+++ server.py
@@ -88,6 +88,11 @@ def brno_mobile_ocr_dataset():
     return output
 
 
+@app.route('/brno_mobile_ocr_dataset/bmod_lines.zip')
+def bmod_lines():
+    return helper.send_file(configuration["brno_mobile_ocr_dataset"]["bmod_lines_zip"])
+
+
 @app.route('/get_handwritten_page', methods=['GET'])
 def get_handwritten_page():
     path = configuration["handwritten_dataset"]["source_directory"]
