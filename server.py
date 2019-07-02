e38238699b1355d162db6fe6635d544899d560ba
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2019-07-02 11:12:48 +0200

    Update BMOD page.

--- server.py
+++ server.py
@@ -90,7 +90,7 @@ def brno_mobile_ocr_dataset():
 
 @app.route('/brno_mobile_ocr_dataset/bmod_lines.zip')
 def bmod_lines():
-    return helper.send_file(configuration["brno_mobile_ocr_dataset"]["bmod_lines_zip"])
+    return helper.send_file(configuration["brno_mobile_ocr_dataset"]["bmod_lines_zip"], as_attachment=True)
 
 
 @app.route('/get_handwritten_page', methods=['GET'])
