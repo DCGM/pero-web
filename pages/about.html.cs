0043319456c2615fdb0e85c94a8c50632a8893d5
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2020-02-12 17:14:27 +0100

    Update about.html.cs

--- pages/about.html.cs
+++ pages/about.html.cs
@@ -11,16 +11,16 @@
         v rámci programu NAKI II na podporu aplikovaného výzkumu a vývoje národní a kulturní identity.
     </p>
     <p>
-        Cílem projektu je vytvořit technologie a nástroje, které zlepší a rozšíří zpřístupnění 
-        historických digitalizovaných dokumentů odborné i laické. 
+        Cílem projektu je vytvořit technologie a nástroje, které zlepší a rozšíří přístupnost 
+        digitalizovaných historických dokumentů odborné i laické veřejnosti. 
         Konkrétně chceme dosáhnout toho, že bude technologicky možné automaticky zpracovat obsah 
-        i špatně čitelných a ručně psaných dokumentů na úrovni, která umožní full-textové vyhledávání jejich obsahu.
-        Vyvíjené nástroje využivají nejnovějších poznatků počítačového vidění, strojového učení a zpracování jazyka.
+        i špatně čitelných a ručně psaných dokumentů na úrovni, která umožní full-textové vyhledávání v rámci jejich obsahu.
+        Vyvíjené nástroje využívají nejnovějších poznatků počítačového vidění, strojového učení a zpracování jazyka.
     </p>
     <p>
         Jednotlivými oblastmi výzkumu v projektu je několik aspektů digitalizačních linek: automatická kontrola a 
-        zlepšování kvality digitalizátů, automatický přepis textu tištěných starších a nekvalitních tištěných dokumentů,
-        automatický a polo-automatický přepis ručně psaného textu a automatická extrakce semantických informací z
+        zlepšování kvality digitalizátů, automatický přepis textu starších tištěných a nekvalitních dokumentů,
+        automatický a polo-automatický přepis ručně psaného textu a automatická extrakce sémantických informací z
         částečně strukturovaných dokumentů, jako jsou například katalogy a matriční knihy.
         Vytvářené nástroje zveřejňujeme jako svobodný a otevřený software a jeho funkčnost ověřujeme 
         při digitalizaci vybraných kolekcí převážně z Moravské zemské knihovny ale i jiných paměťových institucí.
