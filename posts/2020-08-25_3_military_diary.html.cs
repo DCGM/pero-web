855fbe1af3a3ec0ede7702af47187519bf0a43bd
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2020-09-07 11:04:47 +0200

    Add post about military diary

---
+++ posts/2020-08-25_3_military_diary.html.cs
@@ -0,0 +1,21 @@
+{% extends 'template.html.cs' %}
+
+{% block header %}
+    Vojenský deník
+{% endblock %}
+
+{% block perex %}
+    <p>
+		Jednou z organizací, které využívají služeb našeho OCR webu, je také <a style="dotted-link" href="http://www.vhu.cz/">Vojenský Historický Ústav Praha (VHÚ)</a>.
+		Výsledkem této spolupráce je digitalizovaný <a style="dotted-link" href="http://www.digitalniknihovna.cz/dsmo/uuid/uuid:32f6f706-a323-11ea-95c0-001b63bd97ba">vojenský deník</a>, který již byl importován do Digitální studovny Ministerstva Obrany České republiky.
+    </p>
+{% endblock %}
+
+{% block content %}
+	<p>
+		"Muj Deňik", jak se tento dokument nazývá, pochází z období první světové války.
+		Deník byl zpracován pomocí nástrojů vyvinutých v rámci projektu PERO a po importování do Digitální studovny je možné vyhledávat v jeho obsahu, případně si obsah jednotlivých stran deníku stáhnout.
+	</p>
+
+	<img class="img-fluid" src="/static/img/2020-08-25_3_military_diary_image_0.jpg" alt="Ukázka zpracovaného vojenského deníku."/>
+{% endblock %}
