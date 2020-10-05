03f8797ba9d9f8eb8a7c8b1c892bad739bed8989
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2020-10-05 16:08:20 +0200

    Add EGO-DOK project post

---
+++ posts/2020-09-29_1_ego-dok.html.cs
@@ -0,0 +1,18 @@
+{% extends 'template.html.cs' %}
+
+{% block header %}
+    Projekt EGO-DOK
+{% endblock %}
+
+{% block perex %}
+    <p>
+		<a href="http://www.vhu.cz/">Vojenský historický ústav Praha (VHÚ)</a> spustil projekt <a href="http://www.vhu.cz/ego-dok-deniky-vzpominky-a-korespondence-z-prvni-svetove-valky-digitalne-pod-jednou-strechou/?fbclid=IwAR3swGU0AF2u1Pm_l-YTPRwsNM4MvDiet_buu1SSo6L5yNHeJtFIcWspE9E">EGO-DOK</a>, jehož cílem je digitalizovat historické písemnosti.
+		Po získání a naskenování dokumentů od institucí či soukromých osob, jsou získaná data zpracována pomocí nástrojů vyvinutých v rámci projektu PERO.
+    </p>
+{% endblock %}
+
+{% block content %}
+	<p>
+		Výsledky zpracování jsou následně předány majiteli dokumentu a zároveň jsou zvěřejněny v rámci Digitální studovny Ministerstva obrany ČR obdobně jako <a href="https://pero.fit.vutbr.cz/post/2020-08-25_3_military_diary">již zpracované vojenské deníky</a>.
+	</p>
+{% endblock %}
