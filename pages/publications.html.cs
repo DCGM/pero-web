dd008703ff035a9d8ee455f1044f8e46a2e343de
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2021-04-06 09:40:11 +0200

    Add new publications

--- pages/publications.html.cs
+++ pages/publications.html.cs
@@ -5,12 +5,46 @@
 {% endblock %}
 
 {% block content %}
+<div id="publications">
+<h3>2021:</h3>
+<ul>
+    <li>
+        <div class="publication">
+            <div class="publication-text">
+            	Kodym, O. and  Hradiš, M. <span class="publication-emph">Page Layout Analysis System for Unconstrained Historic Documents</span>.
+            </div>
+            <div class="publication-additional-info">
+                <b>Další odkazy:</b>
+                <a class="dotted-link" href="https://arxiv.org/abs/2102.11838v1">arxiv</a>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="publication">
+            <div class="publication-text">
+            	Kohút, J. and  Hradiš, M. <span class="publication-emph">TS-Net: OCR Trained to Switch Between Text Transcription Styles</span>.
+            </div>
+            <div class="publication-additional-info">
+                <b>Další odkazy:</b>
+                <a class="dotted-link" href="https://arxiv.org/abs/2103.05489">arxiv</a>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="publication">
+            <div class="publication-text">
+            	Kišš, M., Beneš, K., and Hradiš, M. <span class="publication-emph">AT-ST: Self-Training Adaptation Strategy for OCR</span>.
+            </div>
+        </div>
+    </li>
+</ul>
+
 <h3>2019:</h3>
 <ul>
     <li>
         <div class="publication" id="b-mod">
-            <div class="publicatioon-text">
-            	Kišš, M., Hradiš, M. and Kodym, O. Brno Mobile OCR Dataset. In: <i>2019 International Conference on Document Analysis and Recognition (ICDAR)</i>. September 2019, p. 1352–1357. ISSN: 1520-5363.
+            <div class="publication-text">
+            	Kišš, M., Hradiš, M. and Kodym, O. <span class="publication-emph">Brno Mobile OCR Dataset</span>. In: <i>2019 International Conference on Document Analysis and Recognition (ICDAR)</i>. September 2019, p. 1352–1357. ISSN: 1520-5363.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
@@ -21,4 +55,5 @@
         </div>
     </li>
 </ul>
+</div>
 {% endblock %}
