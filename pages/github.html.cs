7f8d61ffb0f8542e2436c616234029bf59ed8b79
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2021-04-20 09:36:51 +0200

    Change order of github entries

--- pages/github.html.cs
+++ pages/github.html.cs
@@ -9,11 +9,14 @@
 <p>Během práce na projektu PERO jsme na GitHubu vytvořili několik repozitářů:</p>
 
 <ul>
-	<li><a class="dotted-link" href="https://github.com/DCGM/pero-enhance">PERO Enhance</a> - vylepšení kvality naskenovaných dokumentů</li>
 	<li><a class="dotted-link" href="https://github.com/DCGM/pero-ocr">PERO OCR</a> - automatické zpracování dokumentů</li>
+	<li><a class="dotted-link" href="https://github.com/DCGM/pero_ocr_web">PERO OCR Web</a> - server pro automatické zpracování dokumentů</li>
+</ul>
+
+<ul>
+	<li><a class="dotted-link" href="https://github.com/DCGM/pero-enhance">PERO Enhance</a> - vylepšení kvality naskenovaných dokumentů</li>
 	<li><a class="dotted-link" href="https://github.com/DCGM/pero-quality">PERO Quality</a> - určování kvality naskenovaných dokumentů</li>
 	<li><a class="dotted-link" href="https://github.com/DCGM/pero-web">PERO Web</a> - projektový web</li>
-	<li><a class="dotted-link" href="https://github.com/DCGM/pero_ocr_web">PERO OCR Web</a> - server pro automatické zpracování dokumentů</li>
 	<li><a class="dotted-link" href="https://github.com/DCGM/pero_quality_web">PERO Quality Web</a> - server pro vyhodnocení kvality naskenovaných dokumentů</li>
 </ul>
 
