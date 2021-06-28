0b8302db5144ddda47a37c4b7b96736022ca7fd6
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2021-06-28 12:42:47 +0200

    Update publications

--- pages/publications.html.cs
+++ pages/publications.html.cs
@@ -11,7 +11,7 @@
     <li>
         <div class="publication">
             <div class="publication-text">
-            	Kodym, O. and  Hradiš, M. <span class="publication-emph">Page Layout Analysis System for Unconstrained Historic Documents</span>. Odesláno na double-blind review proces v rámci <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            	Kodym, O. and  Hradiš, M. <span class="publication-emph">Page Layout Analysis System for Unconstrained Historic Documents</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
@@ -22,7 +22,7 @@
     <li>
         <div class="publication">
             <div class="publication-text">
-            	Kohút, J. and  Hradiš, M. <span class="publication-emph">TS-Net: OCR Trained to Switch Between Text Transcription Styles</span>. Odesláno na double-blind review proces v rámci <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            	Kohút, J. and  Hradiš, M. <span class="publication-emph">TS-Net: OCR Trained to Switch Between Text Transcription Styles</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
@@ -33,7 +33,11 @@
     <li>
         <div class="publication">
             <div class="publication-text">
-            	Kišš, M., Beneš, K., and Hradiš, M. <span class="publication-emph">AT-ST: Self-Training Adaptation Strategy for OCR</span>. Odesláno na double-blind review proces v rámci <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            	Kišš, M., Beneš, K., and Hradiš, M. <span class="publication-emph">AT-ST: Self-Training Adaptation Strategy for OCR</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            </div>
+            <div class="publication-additional-info">
+                <b>Další odkazy:</b>
+                <a class="dotted-link" href="https://arxiv.org/abs/2104.13037">arxiv</a>
             </div>
         </div>
     </li>
