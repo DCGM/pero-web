38f0168b486dc4ab648ebadb041dbb2825b94f7c
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2022-03-15 10:35:39 +0100

    Add DAS2022 and HDC posts

---
+++ posts/2022-03-14_1_DAS2022.html.cs
@@ -0,0 +1,18 @@
+{% extends 'template.html.cs' %}
+
+{% block header %}
+    Příspěvek na DAS 2022
+{% endblock %}
+
+{% block perex %}
+    <p>
+        Na konci května 2022 proběhne ve francouzském La Rochelle <a href="https://das2022.univ-lr.fr/">15. ročník International Workshop on Document Analysis System</a> (DAS).
+        Tohoto mezinárodního workshopu se zúčastníme s příspěvkem popisující náš systém, který jsme připravili v rámci soutěže na klasifikaci historických dokumentů na ICDAR 2021 (<a href="https://lme.tf.fau.de/competitions/icdar-2021-competition-on-historical-document-classification/">ICDAR 2021 Competition on Historical Document Classification</a>).
+    </p>
+{% endblock %}
+
+{% block content %}
+    <p>
+        V rámci tohoto článku také zveřejňujeme <a href="/hdc_dataset">rozdělení datových sad</a>, které jsme vytvořili a které je nezbytné pro férové porovnání s dalšími systémy.
+    </p>
+{% endblock %}
---
+++ posts/2022-03-15_1_hdc.html.cs
@@ -0,0 +1,20 @@
+{% extends 'template.html.cs' %}
+
+{% block header %}
+    Klasifikace historických dokumentů
+{% endblock %}
+
+{% block perex %}
+    <p>
+        Konference <a href="https://icdar2021.org/">ICDAR 2021</a> jsme se nezúčastnili pouze s <a href="/post/2021-05-11_1_ICDAR2021">publikovanými články</a>, ale také jsme se zapojili do soutěže na <a href="https://lme.tf.fau.de/competitions/icdar-2021-competition-on-historical-document-classification/">klasifikaci historických dokumentů</a>.
+        Soutěž se skládala ze tří úloh: určování fontu/skriptu, lokalizace a datování dokumentu.
+        Ve všech třech úlohách se náš systém umístil na prvním místě a celou soutěž jsme tak vyhráli.
+    </p>
+{% endblock %}
+
+{% block content %}
+    <p>
+        Při předzpracování poskytnutých datových sad jsme také vytvořili jejich rozdělení na trénovací a validační část.
+        Tato rozdělení společně s krátkým popisem jsou <a href="/hdc_dataset">veřejně k dispozici</a>.
+    </p>
+{% endblock %}
