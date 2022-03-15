9d8512ae4d8eb3a559f8fc4bb0ce312406f76b73
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2022-03-15 10:36:10 +0100

    Update publications page

--- pages/publications.html.cs
+++ pages/publications.html.cs
@@ -6,38 +6,70 @@
 
 {% block content %}
 <div id="publications">
-<h3>2021:</h3>
+<h3>2022:</h3>
 <ul>
     <li>
-        <div class="publication">
+		<div class="publication">
             <div class="publication-text">
-            	Kodym, O. and  Hradiš, M. <span class="publication-emph">Page Layout Analysis System for Unconstrained Historic Documents</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+				Kišš, M., Kohút, J., Beneš, K., and Hradiš, M. <span class="publication-emph">Importance of Textlines in Historical Document Classification</span>. Přijato na <i>2022 International Workshop on Document Analysis System (DAS)</i>.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
-                <a class="dotted-link" href="https://arxiv.org/abs/2102.11838v1">arxiv</a>
+                <a class="dotted-link" href="https://arxiv.org/abs/2201.09575">arxiv</a>,
+                <a class="dotted-link" href="/hdc_dataset">dataset</a>
+            </div>
+        </div>
+    </li>
+</ul>
+
+
+<h3>2021:</h3>
+<ul>
+	<li>
+        <div class="publication">
+            <div class="publication-text">
+            	Kodym, O. and  Hradiš, M. <span class="publication-emph">TG<sup>2</sup>: text-guided transformer GAN for restoring document readability and perceived quality</span>. <i>IJDAR (2021)</i>. https://doi.org/10.1007/s10032-021-00387-z
+            </div>
+            <div class="publication-additional-info">
+                <b>Další odkazy:</b>
+                <a class="dotted-link" href="https://arxiv.org/abs/2102.11838v1">arxiv</a>,
+                <a class="dotted-link" href="https://link.springer.com/article/10.1007/s10032-021-00387-z">Springer</a>
             </div>
         </div>
     </li>
     <li>
         <div class="publication">
             <div class="publication-text">
-            	Kohút, J. and  Hradiš, M. <span class="publication-emph">TS-Net: OCR Trained to Switch Between Text Transcription Styles</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            	Kodym, O. and  Hradiš, M. <span class="publication-emph">Page Layout Analysis System for Unconstrained Historic Documents</span>. In: Lladós J., Lopresti D., Uchida S. (eds) Document Analysis and Recognition – ICDAR 2021. ICDAR 2021. Lecture Notes in Computer Science, vol 12822. Springer, Cham.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
-                <a class="dotted-link" href="https://arxiv.org/abs/2103.05489">arxiv</a>
+                <a class="dotted-link" href="https://arxiv.org/abs/2102.11838v1">arxiv</a>,
+                <a class="dotted-link" href="https://link.springer.com/chapter/10.1007/978-3-030-86331-9_32">Springer</a>
             </div>
         </div>
     </li>
     <li>
         <div class="publication">
             <div class="publication-text">
-            	Kišš, M., Beneš, K., and Hradiš, M. <span class="publication-emph">AT-ST: Self-Training Adaptation Strategy for OCR</span>. Přijato na <i>2021 International Conference on Document Analysis and Recognition (ICDAR)</i>.
+            	Kohút, J. and  Hradiš, M. <span class="publication-emph">TS-Net: OCR Trained to Switch Between Text Transcription Styles</span>. In: Lladós J., Lopresti D., Uchida S. (eds) Document Analysis and Recognition – ICDAR 2021. ICDAR 2021. Lecture Notes in Computer Science, vol 12824. Springer, Cham.
             </div>
             <div class="publication-additional-info">
                 <b>Další odkazy:</b>
-                <a class="dotted-link" href="https://arxiv.org/abs/2104.13037">arxiv</a>
+                <a class="dotted-link" href="https://arxiv.org/abs/2103.05489">arxiv</a>,
+                <a class="dotted-link" href="https://link.springer.com/chapter/10.1007/978-3-030-86337-1_32">Springer</a>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="publication">
+            <div class="publication-text">
+            	Kišš, M., Beneš, K., and Hradiš, M. <span class="publication-emph">AT-ST: Self-Training Adaptation Strategy for OCR</span>. In: Lladós J., Lopresti D., Uchida S. (eds) Document Analysis and Recognition – ICDAR 2021. ICDAR 2021. Lecture Notes in Computer Science, vol 12824. Springer, Cham.
+            </div>
+            <div class="publication-additional-info">
+                <b>Další odkazy:</b>
+                <a class="dotted-link" href="https://arxiv.org/abs/2104.13037">arxiv</a>,
+                <a class="dotted-link" href="https://link.springer.com/chapter/10.1007/978-3-030-86337-1_31">Springer</a>
             </div>
         </div>
     </li>
@@ -54,7 +86,8 @@
                 <b>Další odkazy:</b>
                 <a class="dotted-link" href="https://arxiv.org/abs/1907.01307">arxiv</a>,
                 <a class="dotted-link" href="https://github.com/DCGM/B-MOD">github</a>,
-                <a class="dotted-link" href="https://ieeexplore.ieee.org/document/8978020">IEEEXplore</a>
+                <a class="dotted-link" href="https://ieeexplore.ieee.org/document/8978020">IEEEXplore</a>,
+                <a class="dotted-link" href="/brno_mobile_ocr_dataset">dataset</a>
             </div>
         </div>
     </li>
