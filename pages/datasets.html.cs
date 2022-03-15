26460c1ecf51d4236a27404a6c31bf06638f56c3
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2022-03-15 10:34:42 +0100

    Update datasets page

--- pages/datasets.html.cs
+++ pages/datasets.html.cs
@@ -13,4 +13,7 @@
 
 <h2><a href="/brno_mobile_ocr_dataset">Brno Mobile OCR Dataset</a></h2>
 <p>Datová sada Brno Mobile OCR Dataset je primárně zaměřená na vývoj metod pro rozpoznávání textu v obrázcích s nízkou kvalitou. Datová sada obsahuje původní vyfotografované stránky textu s anotacemi a také jednotlivé vyříznuté řádky. Obrázky obsahují různé úrovně šumu, rozmazání a podobně. <a href="/brno_mobile_ocr_dataset">Více</a></p>
+
+<h2><a href="/hdc_dataset">Klasifikace historických dokumentů</a></h2>
+<p>Zveřejňujeme rozdělení datových sad, které jsme použili při tvorbě systému pro klasifikaci historických dokumentů v rámci soutěže na ICDAR 2021 (<a href="https://lme.tf.fau.de/competitions/icdar-2021-competition-on-historical-document-classification/">ICDAR 2021 Competition on Historical Document Classification</a>). <a href="/hdc_dataset">Více</a></p>
 {% endblock %}
