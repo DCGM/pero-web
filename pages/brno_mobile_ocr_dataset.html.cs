49d84c3ab1fdfcd341f48c772bd833ba32275b21
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-05-02 10:19:22 +0200

    Hide old B-MOD links

--- pages/brno_mobile_ocr_dataset.html.cs
+++ pages/brno_mobile_ocr_dataset.html.cs
@@ -29,6 +29,7 @@ Brno Mobile OCR Dataset
 <h2>Ke stažení</h2>
 Dataset je dostupný na <a class="dotted-link" href="https://doi.org/10.5281/zenodo.15310981">Zenodo</a>.
 
+<!--
 <p style="text-decoration: line-through;">
     <ul>
         <li><a class="dotted-link" href="https://www.fit.vutbr.cz/~ikiss/b-mod/b-mod_templates.zip">Originální šablony</a> (1.45 GB)
@@ -43,6 +44,7 @@ Dataset je dostupný na <a class="dotted-link" href="https://doi.org/10.5281/zen
         </li>
     </ul>
 </p>
+-->
 
 <h2>Ukázky</h2>
 <div class="row">
